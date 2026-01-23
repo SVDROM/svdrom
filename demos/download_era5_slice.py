e777c58ec71a35e3be924121235e784203c417eb
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2026-01-23 17:03:57 +0000

    download u component of wind

--- demos/download_era5_slice.py
+++ demos/download_era5_slice.py
@@ -5,15 +5,16 @@ from utils import (
     write_to_zarr,
 )
 
-era5_path = "gs://gcp-public-data-arco-era5/ar/1959-2022-full_37-1h-0p25deg-chunk-1.zarr-v2"
+era5_path = "gs://weatherbench2/datasets/era5/1959-2022-full_37-1h-0p25deg-chunk-1.zarr-v2"
 era5 = xr.open_zarr(era5_path, chunks={"time": "auto"})
 
-variable = "temperature"
-level = 850
-hours = 6
-start_date, end_date = "2010-01-01", "2022-12-31"
+variable = "u_component_of_wind"
+level = 100
+latitude = slice(90, 30)
+hours = 12
+start_date, end_date = "1980-01-01", "2022-12-31"
 
-era5 = era5[variable].sel(level=level, time=slice(start_date, end_date))
+era5 = era5[variable].sel(level=level, time=slice(start_date, end_date), latitude=latitude)
 era5 = resample_array(era5, hours=hours)
 
 time_periods = generate_time_ranges(start_date, end_date, num_periods=4)
@@ -22,9 +23,9 @@ for period in time_periods:
     era5_slice = era5.sel(time=slice(period[0], period[1]))
     era5_list.append(era5_slice)
 
-filename = f"era5_slice_{start_date}_{end_date}.zarr"
+filename = f"era5_slice_{variable}_{start_date}_{end_date}.zarr"
 folder = "/bask/projects/v/vjgo8416-dmd-ddwm/svdrom/demos/data"
 
 for i, era5_slice in enumerate(era5_list):
-    print(f"Downloading slice {i} out of {len(era5_list)}...")
+    print(f"Downloading slice {i+1} out of {len(era5_list)}...")
     write_to_zarr(era5_slice, filename, folder)
--- demos/download_era5_slice.sh
+++ demos/download_era5_slice.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #SBATCH --qos turing
 #SBATCH --account vjgo8416-dmd-ddwm
-#SBATCH --time 12:00:00
+#SBATCH --time 24:00:00
 #SBATCH --nodes 1
 #SBATCH --ntasks-per-node 1
 #SBATCH --job-name download_era5
