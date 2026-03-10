e85b40248959de2a802c9b0bfd769501c107eefc
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2026-03-10 12:40:08 +0000

    update run_svd.py and job script

--- experiments/run_svd.py
+++ experiments/run_svd.py
@@ -1,10 +1,70 @@
+import pickle
+from pathlib import Path
+
 import xarray as xr
 from dask.distributed import Client
-from svdrom.preprocessing import StandardScaler
+
+import svdrom.config as config
+from svdrom.preprocessing import (
+    StandardScaler,
+    hankel_preprocessing,
+    variable_spatial_stack,
+)
 from svdrom.svd import TruncatedSVD
 
-if __name__ = "__main__":
-    
-    client = Client(processes=False, local_directory="/bask/projects/v/vjgo8416-dmd-ddwm/dask-spill")
-    X = xr.open_dataarray("demos/data/era5_slice_2010-01-01_2022-12-31.zarr", engine="zarr")
-    scaler =
+if __name__ == "__main__":
+    dask_spill_folder = "/bask/projects/v/vjgo8416-dmd-ddwm/dask-spill"
+    era5_slice_path = "data/era5_slice_2010-01-01_2022-12-31.zarr"
+    svd_path = Path("data/svd.pkl")
+    svd_hankel_path = Path("data/svd_hankel.pkl")
+    scaler_path = Path("data/scaler.pkl")
+    n_components = 40
+    n_power_iter = 2
+    n_oversamples = 15
+
+    client = Client(processes=False, local_directory=dask_spill_folder)
+
+    X = xr.open_dataarray(era5_slice_path, chunks="auto")
+    X = X.sel(time=slice("2010", "2019"))
+
+    print("Scaling data...")
+    scaler = StandardScaler()
+    X = scaler(X)
+    scaler_path.parent.mkdir(exist_ok=True)
+    with open(scaler_path, "wb") as f:
+        pickle.dump(scaler, f)
+    print("Scaler object saved to disk.")
+
+    config.set(stack_coord_name="space")
+    X = variable_spatial_stack(X, dims=("latitude", "longitude"))
+    X = X.transpose(("space", "time"))
+
+    print("Computing SVD of standard matrix...")
+    svd = TruncatedSVD(
+        n_components=n_components,
+        algorithm="randomized",
+        rechunk=True,
+        compute_var_ratio=True,
+    )
+    svd.fit(X, n_power_iter=n_power_iter, n_oversamples=n_oversamples)
+    svd_path.parent.mkdir(exist_ok=True)
+    with open(svd_path, "wb") as f:
+        pickle.dump(svd, f)
+    print("SVD object saved to disk.")
+
+    X_hankel = hankel_preprocessing(X)
+
+    print("Computing SVD of Hankel pre-processed matrix...")
+    svd_hankel = TruncatedSVD(
+        n_components=n_components,
+        algorithm="randomized",
+        rechunk=True,
+        compute_var_ratio=True,
+    )
+    svd_hankel.fit(X_hankel, n_power_iter=n_power_iter, n_oversamples=n_oversamples)
+    svd_hankel_path.parent.mkdir(exist_ok=True)
+    with open(svd_hankel_path, "wb") as f:
+        pickle.dump(svd_hankel, f)
+    print("Hankel SVD object saved to disk.")
+
+    client.close()
---
+++ experiments/run_svd.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#SBATCH --qos turing
+#SBATCH --account vjgo8416-dmd-ddwm
+#SBATCH --time 10:00:00
+#SBATCH --nodes 1
+#SBATCH --gpus-per-node 1
+#SBATCH --cpus-per-gpu 10
+#SBATCH --ntasks-per-node 1
+#SBATCH --mem 256G
+#SBATCH --job-name run_svd
+#SBATCH --output run_svd.log
+
+source ../load_python.sh
+
+source ../.venv/bin/activate
+echo $(which python)
+
+python run_svd.py
