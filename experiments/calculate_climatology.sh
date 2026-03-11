5b61d4eae46409a48c0a203bccd509b7b3d30e5d
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2026-03-11 16:29:46 +0000

    job script to calculate climatology

---
+++ experiments/calculate_climatology.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#SBATCH --qos turing
+#SBATCH --account vjgo8416-dmd-ddwm
+#SBATCH --time 6:00:00
+#SBATCH --nodes 1
+#SBATCH --gpus-per-node 1
+#SBATCH --cpus-per-gpu 10
+#SBATCH --ntasks-per-node 1
+#SBATCH --mem 128G
+#SBATCH --job-name calculate_climatology
+#SBATCH --output calculate_climatology.log
+
+source ../load_python.sh
+
+source ../.venv/bin/activate
+echo $(which python)
+
+python calculate_climatology.py
