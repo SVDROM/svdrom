c6c779053e8e20f5ebbb3c0f5fbd0f0f25dc9d88
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2026-03-21 13:00:43 +0000

    request an 8 hour job

--- experiments/calculate_climatology.sh
+++ experiments/calculate_climatology.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #SBATCH --qos turing
 #SBATCH --account vjgo8416-dmd-ddwm
-#SBATCH --time 2:00:00
+#SBATCH --time 8:00:00
 #SBATCH --nodes 1
 #SBATCH --gpus-per-node 1
 #SBATCH --cpus-per-gpu 10
