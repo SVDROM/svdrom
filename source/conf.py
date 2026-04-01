440452032da694db808f832fc52da4f8137db0df
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2026-04-01 21:01:58 +0100

    enable markdown in sphinx

--- source/conf.py
+++ source/conf.py
@@ -13,7 +13,7 @@ author = "David Salvador-Jasin & Robert Vava"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["myst_parser"]
 
 templates_path = ["_templates"]
 exclude_patterns = []
