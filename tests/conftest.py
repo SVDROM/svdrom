9c4819de406eb0c7f6839fd02fa15f0336b4b34b
robertvava <vavarobert10@gmail.com>
2026-05-24 13:25:24 +0100

    removed forced precompilation

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,4 +3,4 @@ from svdrom._pydmd_compat import precompile_pydmd
 
 def pytest_configure() -> None:
     """Precompile PyDMD before pytest imports it with warnings promoted to errors."""
-    precompile_pydmd(force=True)
+    precompile_pydmd()
